220d82aa9efcef1907ff3bc1f0417f3a3d74ed2e
Arthur <arthurbleckrep2@gmail.com>
2024-09-15 17:41:37 -0300

    Adicionando um conversor de temperatura e um CRUD básico

---
+++ Exercicios/Fundamentos/CadastroDeAlunos/Program.cs
@@ -0,0 +1,70 @@
+﻿int controle = 1;
+List<String> alunos = new List<String>();
+
+while (controle != 0)
+{
+    Console.WriteLine("------------------");
+    Console.WriteLine("1 - Cadastrar");
+    Console.WriteLine("2 - Exibir");
+    Console.WriteLine("3 - Atualizar");
+    Console.WriteLine("4 - Deletar");
+    Console.WriteLine("5 - Limpar");
+    Console.WriteLine("6 - Sair");
+    Console.WriteLine("------------------");
+
+    int resp = int.Parse(Console.ReadLine());
+
+    if (resp == 1)
+    {
+        AddAluno(alunos);
+    } else if (resp == 2)
+    {
+        ExibirAlunos(alunos);
+    } else if (resp == 3)
+    {
+        AtualizarAluno(alunos);
+    } else if (resp == 4)
+    {
+        DelAluno(alunos);
+    } else if (resp == 5)
+    {
+        alunos.Clear();
+    }
+    else if (resp == 6)
+    {
+        Console.WriteLine("Programa encerrado!");
+        controle = 0;
+    }
+}
+
+void AddAluno(List<String> alunos)
+{
+    Console.Write("Digite o nome do aluno: ");
+    String resp = Console.ReadLine();
+    alunos.Add(resp);
+}
+
+void ExibirAlunos(List<String> alunos)
+{
+    int i = 0;
+    foreach (String aluno in alunos)
+    {
+        Console.WriteLine($"{i} - {aluno}");
+        i++;
+    }
+}
+
+void AtualizarAluno(List<String> alunos)
+{
+    Console.Write("Digite o número referente ao aluno que deseja alterar: ");
+    int resp = int.Parse(Console.ReadLine());
+    Console.Write("Digite o novo nome: ");
+    alunos[resp] = Console.ReadLine();
+}
+
+void DelAluno(List<String> alunos)
+{
+    Console.Write("Digite o número referente ao aluno que deseja deletar: ");
+    int resp = int.Parse(Console.ReadLine());
+    alunos.RemoveAt(resp);
+}
---
+++ Exercicios/Fundamentos/ConversaoTemperatura/Program.cs
@@ -0,0 +1,38 @@
+﻿int controle = 1;
+
+while (controle != 0)
+{
+    Console.WriteLine("1 - Para converter de Celsius para Fahrenheit");
+    Console.WriteLine("2 - Para converter de Fahrenheit para Celsius");
+    Console.WriteLine("0 - Sair");
+    int resp = int.Parse(Console.ReadLine());
+
+    if (resp == 1)
+    {
+        CelsiusFahrenheit();
+    } else if (resp == 2)
+    {
+        FahrenheitCelsius();
+    } else if (resp == 0)
+    {
+        Console.WriteLine("Programa encerrado!");
+        controle = 0;
+    } else
+    {
+        Console.WriteLine("Opção inválida!");
+    }
+}
+
+void CelsiusFahrenheit()
+{
+    Console.Write("Digite a temperatura em Celsius: ");
+    int celsius = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Temperatura em Fahrenheit: {(celsius * 9 / 5 + 32)}");
+}
+
+void FahrenheitCelsius()
+{
+    Console.Write("Digite a temperatura em Fahrenheit: ");
+    int fahrenheit = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Temperatura em Celsius: {((fahrenheit - 32) / 1.8)}");
+}
