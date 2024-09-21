1d794f8d80e743751e5328c7cd445e7b1456f2b9
Arthur <arthurbleckrep2@gmail.com>
2024-09-21 17:14:43 -0300

    Removendo alguns exercícios

--- Exercicios/Fundamentos/ConversaoTemperatura/obj/Debug/net8.0/ConversaoTemperatura.AssemblyInfo.cs
+++ Exercicios/Fundamentos/ConversaoTemperatura/obj/Debug/net8.0/ConversaoTemperatura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversaoTemperatura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040d564ab2139c4141eb84589087a905a342c1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713bed911cf45fd992405ba7a07aaab0eabf7f71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversaoTemperatura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversaoTemperatura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
