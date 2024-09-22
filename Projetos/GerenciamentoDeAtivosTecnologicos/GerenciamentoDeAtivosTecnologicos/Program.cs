f4f89b2c8d0d911458581946a2d08f01bea49db6
Arthur <arthurbleckrep2@gmail.com>
2024-09-22 17:56:47 -0300

    Adicionando o CRUD para os notebooks

--- Projetos/GerenciamentoDeAtivosTecnologicos/GerenciamentoDeAtivosTecnologicos/Program.cs
+++ Projetos/GerenciamentoDeAtivosTecnologicos/GerenciamentoDeAtivosTecnologicos/Program.cs
@@ -7,13 +7,57 @@ namespace GerenciamentoDeAtivosTecnologicos
         public static void Main(string[] args)
         {
             DispositivosRepositorio dispositivosRepositorio = new DispositivosRepositorio();
+            Notebook notebook = null;
 
-            Notebook notebook = new Notebook(0, "Dell", "Inspiron", 2021, "Notebook", "Intel Core i5", "8 GB", "SSD 256 GB", "Windows 10 Home");
-            Notebook notebook1 = new Notebook(1, "MacBook", "A2338", 2020, "Notebook", "Apple M1", "8 GB", "SSD 256 GB", "macOS Big Sur");
+            int respMenu = 0;
+            do
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("1 - Notebook");
+                Console.WriteLine("2 - Monitor");
+                Console.WriteLine("3 - Impressora");
+                Console.WriteLine("4 - Teclado");
+                Console.WriteLine("5 - Mouse");
+                Console.WriteLine("6 - Sair");
+                Console.WriteLine("---------------------------");
 
-            dispositivosRepositorio.AddNotebook(notebook);
-            dispositivosRepositorio.AddNotebook(notebook1);
-            dispositivosRepositorio.ExibirNotebooks();
+                respMenu = int.Parse(Console.ReadLine());
+
+                switch (respMenu)
+                {
+                    case 1:
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine("1 - Adicionar");
+                        Console.WriteLine("2 - Exibir");
+                        Console.WriteLine("3 - Atualizar");
+                        Console.WriteLine("4 - Remover");
+                        Console.WriteLine("---------------------------");
+
+                        int respNotebook = int.Parse(Console.ReadLine());
+
+                        switch (respNotebook)
+                        {
+                            case 1:
+                                notebook = new Notebook(0, "Dell 15 3000", "Inspiron", 2021, "Notebook", "Intel Core i5", "8 GB", "SSD 256 GB", "Windows 10 Home");
+                                dispositivosRepositorio.AddNotebook(notebook);
+                                break;
+                            case 2:
+                                dispositivosRepositorio.ExibirNotebooks();
+                                break;
+                            case 3:
+                                notebook = new Notebook(1, "MacBook Pro 13", "A2338", 2020, "Notebook", "Apple M1", "8 GB", "SSD 256 GB", "macOS Big Sur");
+                                dispositivosRepositorio.AtualizarNotebook(0, notebook);
+                                break;
+                            case 4:
+                                dispositivosRepositorio.DelNotebook(0);
+                                break;
+                            default:
+                                Console.WriteLine("Valor inválido!");
+                                break;
+                        }
+                        break;
+                }
+            } while (respMenu != 6);
             
         }
     }
--- Projetos/GerenciamentoDeAtivosTecnologicos/GerenciamentoDeAtivosTecnologicos/obj/Debug/net8.0/GerenciamentoDeAtivosTecnologicos.AssemblyInfo.cs
+++ Projetos/GerenciamentoDeAtivosTecnologicos/GerenciamentoDeAtivosTecnologicos/obj/Debug/net8.0/GerenciamentoDeAtivosTecnologicos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoDeAtivosTecnologicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf1285d6cc2c20f13e13f9da621801759077137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0757d8b19546880212c514ae24d215c83094e1dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoDeAtivosTecnologicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoDeAtivosTecnologicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
